a4e3a3e07160020973d717bd5e2e4cafc653ad62
Abdul Rahim <raheemkhan.rk12345@gmail.com>
2026-07-31 16:05:16 +0500

    Update vite.config.js

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/Mini_project1/',
+  base: '/web-development-practice/',
 })
